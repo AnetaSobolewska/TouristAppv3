3e076007272eb8efb75aa191407f13cf547546ba
jakub9 <jakub.krzysztof.czapski@gmail.com>
2014-11-15 21:03:23 +0100

    added appbar icon, added ImageURL prop to PlaceModel

--- TouristAppv2/Model/PlaceModel.cs
+++ TouristAppv2/Model/PlaceModel.cs
@@ -11,7 +11,13 @@ namespace TouristAppv2.Model
         private string _adress;
         private string _name;
         private string _description;
+        private string _imageUrl;
 
+        public string ImageURL
+        {
+            get { return _imageUrl; }
+            set { _imageUrl = value; }
+        }
 
 
         public string Description
