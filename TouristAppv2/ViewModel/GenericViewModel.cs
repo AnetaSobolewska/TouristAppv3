66f75e2511cc89f29a1e4d5bf7014e00fe96900a
jakub9 <jakub.krzysztof.czapski@gmail.com>
2014-11-16 21:56:32 +0100

    xml file loaded to app

--- TouristAppv2/ViewModel/GenericViewModel.cs
+++ TouristAppv2/ViewModel/GenericViewModel.cs
@@ -23,27 +23,110 @@ namespace TouristAppv2.ViewModel
        
         private string _selectedcategory = MainViewModel.SelectedCategory;
         private string _description;
+        private string _pic;
+        private string _address;
 
 
         public GenericViewModel()
         {
             XDocument xdoc = XDocument.Load("Places.xml");
             XAttribute name = xdoc.Descendants("place").ElementAt(0).Attribute("name");
+            XAttribute Cname = xdoc.Descendants("place").ElementAt(1).Attribute("name");
+            XAttribute Comwellname = xdoc.Descendants("place").ElementAt(2).Attribute("name");
+            XAttribute Prindsenname = xdoc.Descendants("place").ElementAt(3).Attribute("name");
+
             if (_selectedcategory==name.Value)
             {
                 PlaceName = name.Value;
                 IEnumerable<XElement> descriptionofplace = xdoc.Descendants("place").ElementAt(0).Descendants("description");
+
                 foreach (XElement descElement in descriptionofplace)
                 {
                     Description = descElement.Value;
                 }
+                IEnumerable<XElement> picofplace = xdoc.Descendants("place").ElementAt(0).Descendants("ImageURL");
+                foreach (XElement picelement in picofplace)
+                {
+                    Pic = picelement.Value;
+                }
+                IEnumerable<XElement> addressElement = xdoc.Descendants("place").ElementAt(0).Descendants("address");
+                foreach (XElement adress in addressElement)
+                {
+                    Address = adress.Value;
+                }
                
-
             }
             
+            if (_selectedcategory == Cname.Value)
+                {
+                    PlaceName = Cname.Value;
+                    IEnumerable<XElement> cathedraldesc = xdoc.Descendants("place").ElementAt(1).Descendants("description");
+                    foreach (XElement Celement in cathedraldesc)
+                    {
+                        Description = Celement.Value;
+                    }
+                    IEnumerable<XElement> picElements = xdoc.Descendants("place").ElementAt(1).Descendants("ImageURL");
+                    foreach (XElement picElement in picElements)
+                    {
+                        Pic = picElement.Value;
+                    } IEnumerable<XElement> addressElements = xdoc.Descendants("place").ElementAt(1).Descendants("addres");
+                    foreach (XElement addresElement in addressElements)
+                    {
+                        Address = addresElement.Value;
+                    }
 
+                }
 
+            if (_selectedcategory == Comwellname.Value)
+            {
+                PlaceName = Comwellname.Value;
+                IEnumerable<XElement> comwellElements = xdoc.Descendants("place").ElementAt(2).Descendants("description");
+                foreach (XElement comwellElement in comwellElements)
+                {
+                    Description = comwellElement.Value;
+                }
+                IEnumerable<XElement> comwellpicElements = xdoc.Descendants("place").ElementAt(2).Descendants("ImageURL");
+                foreach (XElement comwellpicElement in comwellpicElements)
+                {
+                    Pic = comwellpicElement.Value;
+                } IEnumerable<XElement> comwelladdressElements = xdoc.Descendants("place").ElementAt(2).Descendants("address");
+                foreach (XElement comwelladdressElement in comwelladdressElements)
+                {
+                    Address = comwelladdressElement.Value;
+                }
 
+            }
+            if (_selectedcategory == Prindsenname.Value)
+            {
+                PlaceName = Prindsenname.Value;
+                IEnumerable<XElement> prindsebdescriptionElements = xdoc.Descendants("place").ElementAt(3).Descendants("description");
+                foreach (XElement prindsendesciptionElement in prindsebdescriptionElements)
+                {
+                    Description = prindsendesciptionElement.Value;
+                }
+                IEnumerable<XElement> prindsenpicElements = xdoc.Descendants("place").ElementAt(3).Descendants("ImageURL");
+                foreach (XElement prindsenpicElement in prindsenpicElements)
+                {
+                    Pic = prindsenpicElement.Value;
+                } IEnumerable<XElement> prindsenaddressElements = xdoc.Descendants("place").ElementAt(3).Descendants("address");
+                foreach (XElement prindsenaddressElement in prindsenaddressElements)
+                {
+                    Address =prindsenaddressElement.Value;
+                }
+
+            }
+        }
+
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        public string Pic
+        {
+            get { return _pic; }
+            set { _pic = value; }
         }
 
         public string Description
