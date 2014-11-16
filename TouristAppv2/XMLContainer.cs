0737d41eb111d4c80936aad33ad79c209f6fa978
AnetaSobolewska <aneta.edyta.sobolewska@gmail.com>
2014-11-16 09:11:56 +0100

    restauracje do xml i klasa XMLContainer i GenericViewModel

---
+++ TouristAppv2/XMLContainer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Windows.Storage;
+using TouristAppv2.Model;
+using TouristAppv2.View;
+using TouristAppv2.ViewModel;
+
+namespace TouristAppv2
+{
+    class XMLContainer
+    {
+       
+        
+       public string place = MainViewModel.SelectedCategory;
+       
+        public XMLContainer()
+        {
+            GetXML("Places");
+        }
+
+        private async void GetXML(string Places)
+        {
+            StorageFile placesFile = null;
+
+            if(Places=="Places")
+            {
+                try
+                {
+                    placesFile = await ApplicationData.Current.LocalFolder.GetFileAsync("Places.xml");
+                }
+                catch (Exception)
+                {
+                    
+                    
+                }
+                
+            }
+        }
+    }
+}
